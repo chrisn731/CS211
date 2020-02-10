fc8d55bd010dfc3e5eec46346baab638ee7765c3
Christopher M. Naporlee <chrisn731@gmail.com>
2020-02-10 01:12:58 -0500

    pushing first assignment directories

---
+++ pa1/pa1/src/roman/roman.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char * convert(int numtoconvert) {
+
+
+	return romanNum;
+}
+
+
+int main(int argc, char** argv) {
+	if(argc == 0) return 0;
+	char** input = argv;
+	int numtoconvert = atoi(argv[1]);
+
+	
+	
+}
