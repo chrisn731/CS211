4ce6820cf1e025e7522a9e3d8dab54f97cb9efd2
Chris Naporlee <chrisn731@gmail.com>
2020-02-12 03:13:23 -0500

    list prototype file

---
+++ pa1/pa1/src/list/list.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int key;  
+    struct Node *next;
+};
+
+struct Node *createNode(int value, struct Node *next) {
+    struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
+    newnode->key = value;
+    newnode->next = next;
+    return newnode;
+}
+
+int insert(int value, struct Node *head){
+    if(head == NULL){
+        head = createNode(value, NULL);
+        return 1;
+    }
+    struct Node *curr = head;
+    struct Node *prev;
+    while(curr != NULL && curr->key >= value){
+        if(curr->key == value) return 1;
+
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if(curr == NULL){
+        prev->next = createNode(value, NULL);
+    } else {
+        prev->next = createNode(value, curr);
+    }
+    return 1;
+}
+
+int delete(int value, struct Node *head){
+    if(head == NULL){
+        return 1;
+    }
+    if(head->key == value){
+        head = NULL;
+    }
+
+    struct Node *curr = head;
+    struct Node *prev;
+
+    while(curr != NULL && curr->key != value){
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if(curr == NULL) return 1;
+
+    prev->next = curr->next;
+    free(curr);
+}
+
+void printlist(struct Node *head){
+    struct Node *curr;
+    for(curr = head; curr != NULL; curr = curr->next){
+        printf(" %i", curr->key);
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    char op;
+    int value, listlength;
+    listlength = 0;
+    struct Node *head;
+
+    while(scanf("%c %i", &op, &value) != EOF){
+
+        if(op == 'i'){
+
+            if(insert(value, head)){
+                listlength++;
+                printf("%i :", listlength);
+                printlist(head);
+            } else {
+                //Error Handle here
+                printf("\n");
+            }
+
+        } else if (op == 'd') {
+
+            if(delete(value, head)){
+                listlength--;
+                printf("%i :", listlength);
+                printlist(head);
+            } else {
+                //Error Handle here
+                printf("\n");
+            }
+
+        }
+    }
+    return 0;
+}
