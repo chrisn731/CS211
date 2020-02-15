06ca95591f8fffabb5c84f7934d80b648c664c1a
Chris Naporlee <chrisn731@gmail.com>
2020-02-14 20:31:33 -0500

    holy mother fuckign fuck it really was buggin about a SPACE WHRFFDFDJS

--- pa1/pa1/src/mexp/mexp.c
+++ pa1/pa1/src/mexp/mexp.c
@@ -1,132 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int dimension=0; /*global variable for dimension of the matrix*/
+void printmatrix(int **matrix, int size);
 
-/*a function that takes in 2 2D pointers, and multiplies them by each other. Works if the inputs follow
-	the dimension restrictions of mtrix multiplcation*
-			As this is a matrix exponentiation file, and the matrix is square, it should 
-			always follow restrictions, and just multiplication should be done*/
-
-int ** multiply (int ** matrixa, int** matrixb) {
-
-	int total=0;
-
-	/*allocate a product matrix*/
-	int ** product = (int **)malloc(sizeof(int *) * dimension);
+int **createMatrix(int size){
+	int **newmatrix = malloc(size * sizeof(int*));
 	int i;
-	for (i =0; i<dimension; i++) {
-		product[i] = (int *)malloc(sizeof(int)* dimension);
+	for(i = 0; i < size; i++){
+		newmatrix[i] = malloc(size * sizeof(int));
 	}
-
-	int y, p, x;
-	for (y=0; y< dimension; y++) {//goes down first
-		for (p=0; p< dimension; p++) {//goes across second
-			for (x=0; x< dimension; x++)	{//goes across first
-		
-				total= total+ matrixa [y][x] * matrixb[x][p];
-		
-			}
-
-			product[y][p] = total;
-			total=0;
-		
-		}
-	}
-
-	free(matrixb);
-		
-	return product;
+	return newmatrix;
 }
 
-
-
-
-
-int main (int argc, char** argv) {
-
-	/*opens a file from the argument*/
-	FILE *fp;
-	int size = 0;
-	for (size=0; argv[1][size]!='\0'; size++) {
-	};
-	size++;
-
-	char * filename = (char *)malloc(sizeof(char)*size);
-	strcpy(filename, argv[1]);
-
-	fp = fopen(filename, "r");
-
-
-
-	/*if no file print error*/
-	if (!fp) {
-		printf("error\n");
-		return 0;
-	}
-
-	/*put the dimension of the square matrix into the variable*/
-	fscanf(fp, "%d\n", &dimension);
-	
-
-	/*allocate memory for a 2D matrix, both the original and current result*/
-		/*while being multipled, the result holds the current answer, and matrix holds the original*/
-
-	int ** matrix = (int ** )malloc(sizeof(int *) * dimension);
-	int ** result = (int ** )malloc(sizeof(int *) * dimension);
+void freeMatrix(int ***matrix, int size){
 	int i;
-	for (i=0; i<dimension; i++) {
-		result[i] = (int*)malloc(sizeof(int) * dimension);
-		matrix[i] = (int*)malloc(sizeof(int) * dimension);
+	for(i = 0; i < size; i++){
+		free((*matrix)[i]);
 	}
-	int j;
-	/*read the file and put the data into the matrix*/
-	for (i=0; i<dimension; i++){
-		for (j=0; j<dimension; j++) {
-			fscanf(fp, "%d", &matrix[i][j]);
-			result[i][j] = matrix [i][j];
+}
+
+void setmatrix(int ***matrixA, int ***matrixB, int size){
+	int i,j;
+	for(i = 0; i < size; i++){
+		for(j = 0; j < size; j++){
+			(*matrixA)[i][j] = (*matrixB)[i][j];
 		}
 	}
+}
 
-	/*read the power of the exponent*/
-	int power =0;
-	fscanf(fp, "%d", &power);
+int matrixmulti(int **matrixA, int power, int size){
+	
+	int **product = createMatrix(size);
+	int **matrixB = createMatrix(size);
 
-	/*if the zero power, return the identity matrix*/
-	if (power==0) {
-		for (i=0; i<dimension; i++){
-			for (j=0; j<dimension; j++) {
-				if (i==j) {
-					result[i][j] = 1;
-				}else {
-					result[i][j] = 0;
+	int row, col, colplace, result = 0;
+
+	setmatrix(&matrixB, &matrixA, size);
+	
+	/**
+	 * row = which row to select the element from
+	 * col = which column in the matrix
+	 * colplace = which vector of A to select for 
+	 * matrix[row][col] will be used as the horizontal vector of A
+	 * matrix[col][colplace] will be used as the vertical vector of A
+	 */
+	while( (power - 1) > 0){
+		for(row = 0; row < size; row++){
+			for(colplace = 0; colplace < size; colplace++){
+				for(col = 0; col < size; col++){
+					result += matrixA[row][col] * matrixB[col][colplace];
 				}
-				
+				product[row][colplace] = result;
+				result = 0;
 			}
-		} 
+		}
+		power--;
+		setmatrix(&matrixA, &product, size);
 	}
+	freeMatrix(&matrixA, size);
+	freeMatrix(&matrixB, size);
 
-	/*call the multiply functon power-1 times*/
-	for (i=1; i<power; i++) {
+	printmatrix(product, size);
+	freeMatrix(&product, size);
+	return 1;
+}
 
-		result = multiply(matrix, result);	
-	}
-	free(matrix);
-
-
-
-	/*print the result*/
-	for (i=0; i<dimension; i++){
-		for (j=0; j<dimension; j++) {
-			printf("%d", result[i][j]);
-			if (j<(dimension-1)) printf(" ");
+void IMatrix(int size){
+	int i,j;
+	for(i = 0; i < size; i++){
+		for(j = 0; j < size; j++){
+			if(i == j){
+				printf("1 ");
+			} else {
+				printf("0 ");
+			}
 		}
 		printf("\n");
 	}
+}
 
+void printmatrix(int **matrix, int size){
+	int i,j;
+	for(i = 0; i < size; i++){
+		for(j = 0; j < size; j++) {
+			printf("%i", matrix[i][j]);
+            if(!(j+1 == size)) printf(" ");
+		}
+		printf("\n");
+	}
+}
 
-	fclose(fp);
+int main() {
+
+	int size, power;
+
+	if(!scanf("%i", &size)){
+		return EXIT_FAILURE;
+	}
+	int **matrix = createMatrix(size);
+
+	int i, j;
+
+	for(i = 0; i < size; i++){
+		for(j = 0; j < size; j++){
+			if(!scanf("%i", &matrix[i][j])) {
+				puts("error");
+				return EXIT_FAILURE;
+			}
+		}
+	}
+
+	if(!scanf("%i", &power)){
+		return EXIT_FAILURE;
+	}
+
+	if(power == 0){
+		IMatrix(size);
+	} else if(power < 0){
+		return EXIT_FAILURE;
+	} else if(!matrixmulti(matrix, power, size)){
+		return EXIT_FAILURE;
+	}
 
 	return 0;
 }
