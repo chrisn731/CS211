b230aad83f42a346840d69ec73fb82ecd9764fd9
Chris Naporlee <chrisn731@gmail.com>
2020-02-14 20:29:30 -0500

    another shitty test for another shitty program

--- pa1/pa1/src/mexp/mexp.c
+++ pa1/pa1/src/mexp/mexp.c
@@ -1,108 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include <string.h>
 
+int dimension=0; /*global variable for dimension of the matrix*/
 
-void arr_free(int **arr, int dim) {
-    int i;
-    for(i = 0; i < dim; i++)
-        free(arr[i]);
-    free(arr);
+/*a function that takes in 2 2D pointers, and multiplies them by each other. Works if the inputs follow
+	the dimension restrictions of mtrix multiplcation*
+			As this is a matrix exponentiation file, and the matrix is square, it should 
+			always follow restrictions, and just multiplication should be done*/
+
+int ** multiply (int ** matrixa, int** matrixb) {
+
+	int total=0;
+
+	/*allocate a product matrix*/
+	int ** product = (int **)malloc(sizeof(int *) * dimension);
+	int i;
+	for (i =0; i<dimension; i++) {
+		product[i] = (int *)malloc(sizeof(int)* dimension);
+	}
+
+	int y, p, x;
+	for (y=0; y< dimension; y++) {//goes down first
+		for (p=0; p< dimension; p++) {//goes across second
+			for (x=0; x< dimension; x++)	{//goes across first
+		
+				total= total+ matrixa [y][x] * matrixb[x][p];
+		
+			}
+
+			product[y][p] = total;
+			total=0;
+		
+		}
+	}
+
+	free(matrixb);
+		
+	return product;
 }
 
 
-void arr_print(int **arr, int dim) {
-    int i, j; 
-    for(i = 0; i < dim; i++) {
-        for(j = 0; j < dim; j++) 
-            printf((j == dim - 1) ? "%d" : "%d ", arr[i][j]);
-        printf("\n");
-    }
-}
 
 
-void exponentiate(int **arr, int exp, int dim) {
-    int i, j, k, n;
-    if(!exp) {
-        for(i = 0; i < dim; i++)
-            for(j = 0; j < dim; j++)
-                arr[i][j] = (i == j) ? 1 : 0;
-        return;
-    }
-    
-    int base[dim][dim]; 
-    for(i = 0; i < dim; i++)
-        for(j = 0; j < dim; j++)
-            base[i][j] = arr[i][j];
-    
-    for(n = 1; n < exp; n++) {
-        int mul[dim][dim];
-        /** memset(mul, 0, sizeof(int) * dim * dim); */
-        
-        for(i = 0; i < dim; i++) {
-            for(j = 0; j < dim; j++) {
-                mul[i][j] = 0; 
-                for(k = 0; k < dim; k++)
-                    mul[i][j] += arr[i][k] * base[k][j];
-            } 
-        }
-        for(i = 0; i < dim; i++)
-            for(j = 0; j < dim; j++)
-                arr[i][j] = mul[i][j];
-    }
-}
+
+int main (int argc, char** argv) {
+
+	/*opens a file from the argument*/
+	FILE *fp;
+	int size = 0;
+	for (size=0; argv[1][size]!='\0'; size++) {
+	};
+	size++;
+
+	char * filename = (char *)malloc(sizeof(char)*size);
+	strcpy(filename, argv[1]);
+
+	fp = fopen(filename, "r");
 
 
-int main(int argc, char **argv) {
-    if(argc < 2) {
-        perror("Need file\n");
-        return -1;
-    }
-    
-    char *c = NULL;
-    size_t n = 0;
-    FILE *fd = fopen(argv[1], "r");
-    
-    int dim, exp;
-    if(getline(&c, &n, fd)) {
-        dim = atoi(c); 
-    } else {
-        perror("Cannot get dimension\n");
-        return -1;
-    }
 
-    int **arr = (int **) malloc(dim * sizeof(int *));
-    int i;
-    for(i = 0; i < dim; i++)
-        arr[i] = (int *) malloc(dim * sizeof(int));
+	/*if no file print error*/
+	if (!fp) {
+		printf("error\n");
+		return 0;
+	}
 
-    for(i = 0; i < dim; i++) {
-        int j = 0; 
-        if(getline(&c, &n, fd) != -1) {
-            char *ptr = strtok(c, " ");
-            while(ptr) {
-                arr[i][j] = atoi(ptr);
-                ptr = strtok(NULL, " ");
-                j++;
-            }
-        }
-    }
+	/*put the dimension of the square matrix into the variable*/
+	fscanf(fp, "%d\n", &dimension);
+	
 
-    if(getline(&c, &n, fd)) {
-        exp = atoi(c);
-    } else {
-        perror("Cannot get exponent\n");
-        return 1;
-    }
-    
-    exponentiate(arr, exp, dim);
-    arr_print(arr, dim);
-    arr_free(arr, dim);
-    fclose(fd);
-    free(c);
-    return 0;
+	/*allocate memory for a 2D matrix, both the original and current result*/
+		/*while being multipled, the result holds the current answer, and matrix holds the original*/
+
+	int ** matrix = (int ** )malloc(sizeof(int *) * dimension);
+	int ** result = (int ** )malloc(sizeof(int *) * dimension);
+	int i;
+	for (i=0; i<dimension; i++) {
+		result[i] = (int*)malloc(sizeof(int) * dimension);
+		matrix[i] = (int*)malloc(sizeof(int) * dimension);
+	}
+	int j;
+	/*read the file and put the data into the matrix*/
+	for (i=0; i<dimension; i++){
+		for (j=0; j<dimension; j++) {
+			fscanf(fp, "%d", &matrix[i][j]);
+			result[i][j] = matrix [i][j];
+		}
+	}
+
+	/*read the power of the exponent*/
+	int power =0;
+	fscanf(fp, "%d", &power);
+
+	/*if the zero power, return the identity matrix*/
+	if (power==0) {
+		for (i=0; i<dimension; i++){
+			for (j=0; j<dimension; j++) {
+				if (i==j) {
+					result[i][j] = 1;
+				}else {
+					result[i][j] = 0;
+				}
+				
+			}
+		} 
+	}
+
+	/*call the multiply functon power-1 times*/
+	for (i=1; i<power; i++) {
+
+		result = multiply(matrix, result);	
+	}
+	free(matrix);
+
+
+
+	/*print the result*/
+	for (i=0; i<dimension; i++){
+		for (j=0; j<dimension; j++) {
+			printf("%d", result[i][j]);
+			if (j<(dimension-1)) printf(" ");
+		}
+		printf("\n");
+	}
+
+
+	fclose(fp);
+
+	return 0;
 }
