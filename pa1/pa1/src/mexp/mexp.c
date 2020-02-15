a5a610dcf336f2ab5a80a1a5a91e39aff1727177
Chris Naporlee <chrisn731@gmail.com>
2020-02-14 21:14:26 -0500

    holy mother fuck it actually works

--- pa1/pa1/src/mexp/mexp.c
+++ pa1/pa1/src/mexp/mexp.c
@@ -83,7 +83,7 @@ void printmatrix(int **matrix, int size){
 	int i,j;
 	for(i = 0; i < size; i++){
 		for(j = 0; j < size; j++) {
-			printf("%i", matrix[i][j]);
+			printf("%i ", matrix[i][j]);
 		}
 		printf("\n");
 	}
