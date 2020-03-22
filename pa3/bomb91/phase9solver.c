2fc370c638c1914fb0f2891b5d0a4e878d114c36
Chris Naporlee <chrisn731@gmail.com>
2020-03-22 01:27:00 -0400

    Added an Automated Solver and Manual Mode Solver

--- pa3/bomb91/phase9solver.c
+++ pa3/bomb91/phase9solver.c
@@ -35,27 +35,91 @@ int fun9(struct Node *root, int val){
     }
 }
 
+struct Node* Build_Tree(int *values, struct Node *root) {
+    int i = 0;
+    while(i != 15) {
+        int key = values[i];
+        if(root == NULL) {
+            root = New_Node(key);
+        }
+        else { 
+            struct Node *temp = root, *prev;
+            while(temp != NULL){
+                prev = temp;
+                if(temp->value == key){
+                    return 0;
+                } else {
+                    temp = (temp->value > key) ? temp->left : temp->right;
+                }
+            }
+
+            if(prev->value > key){
+                prev->left = New_Node(key);
+            } else {
+                prev->right = New_Node(key);
+            }
+        }
+        ++i;
+    }
+    return root;
+}
+
+
+struct Node* Create_Tree(struct Node *root) {
+    printf("Will you be inputting values hex? (yes/no): ");
+    char inputtype[4];
+    scanf(" %[^\n]%*c", inputtype);
+    int INHEX = 0;
+    if(inputtype[0] == 'y'){
+        INHEX = 1;
+    }
+    int Nodes[] = {1, 21, 22, 31, 32, 33, 34, 41, 42, 43, 44, 45, 46, 47, 48};
+    unsigned int inputval;
+    int values[14];
+    int i = 0;
+    while(i != 15) {
+        printf("Please input value for n %i: ", Nodes[i]);
+        if(INHEX) {
+            while(!scanf("%x", &inputval));
+        } else {
+            while(!scanf("%i", &inputval));
+        }
+        values[i] = inputval;
+        ++i;
+    }
+    root = Build_Tree(values, root);
+    return root;
+}
+
 
 int main(){
-    struct Node *root = New_Node(36);
+    printf("(A)utomated or (M)anual?: ");
+    char input;
+    scanf("%c", &input);
+    struct Node *root = NULL;
+    if(input == 'A') {
+        root = Create_Tree(root);
+    } else {
+        root = New_Node(36);
 
-    root->left = New_Node(8);
-    root->right = New_Node(50);
+        root->left = New_Node(8);
+        root->right = New_Node(50);
 
-    root->left->left = New_Node(6);
-    root->right->right = New_Node(107);
-    root->left->right = New_Node(22);
-    root->right->left = New_Node(45);
+        root->left->left = New_Node(6);
+        root->right->right = New_Node(107);
+        root->left->right = New_Node(22);
+        root->right->left = New_Node(45);
 
-    root->left->left->left = New_Node(1);
-    root->left->left->right = New_Node(7);
-    root->left->right->left = New_Node(20);
-    root->left->right->right = New_Node(35);
+        root->left->left->left = New_Node(1);
+        root->left->left->right = New_Node(7);
+        root->left->right->left = New_Node(20);
+        root->left->right->right = New_Node(35);
 
-    root->right->left->left = New_Node(40);
-    root->right->left->right = New_Node(47);
-    root->right->right->left = New_Node(63);
-    root->right->right->right = New_Node(1001);
+        root->right->left->left = New_Node(40);
+        root->right->left->right = New_Node(47);
+        root->right->right->left = New_Node(63);
+        root->right->right->right = New_Node(1001);
+    }
 
     eax = 0;
 
@@ -63,7 +127,7 @@ int main(){
     for(i = 1; i < 1002; ++i){
         eax = fun9(root, i);
         if(eax == 2) {
-            printf("%d", i);
+            printf("The Answer to defuse phase 9 is: %d", i);
             exit(EXIT_SUCCESS);
         }
     }
